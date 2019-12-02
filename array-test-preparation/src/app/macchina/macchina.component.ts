8ebe6f641e50f7ebe4215adbbe62f10c6f278ef4
gioele-bra <brancaleoni.gioele@iisgalvanimi.edu.it>
2019-12-02 12:31:45 +0000

    Preparazione Verifica Array | Punto 4-5

--- array-test-preparation/src/app/macchina/macchina.component.ts
+++ array-test-preparation/src/app/macchina/macchina.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-macchina',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./macchina.component.css']
 })
 export class MacchinaComponent implements OnInit {
+
+  @Input() name: string;
 
   constructor() { }
 
